e0c5726db6caacc87f7c880b05b36a5f7e93e74a
brokenboys <shrivastavaabhishek687@gmail.com>
2025-07-17 17:19:18 +0530

    Update 0_AADI-JOKES.js

--- Priyansh/commands/0_AADI-JOKES.js
+++ Priyansh/commands/0_AADI-JOKES.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 module.exports.config = {
-	name: "Malik",
+	name: "Malikin",
     version: "1.1.1",
 	hasPermssion: 0,
 	credits: "AADI BABU",///don't change my Credit Coz i Edit 
