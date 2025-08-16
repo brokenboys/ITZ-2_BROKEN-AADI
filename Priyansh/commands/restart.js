55a280bd6355aeacbdf2e67996179e1671a32e46
brokenboys <shrivastavaabhishek687@gmail.com>
2025-08-16 16:24:23 +0530

    Update restart.js

--- Priyansh/commands/restart.js
+++ Priyansh/commands/restart.js
@@ -1,9 +1,9 @@
 module.exports.config = {
 	name: "restart",
 	version: "1.0.0",
-	hasPermssion: 2,
-	credits: "𝙋𝙧𝙞𝙮𝙖𝙣𝙨𝙝 𝙍𝙖𝙟𝙥𝙪𝙩",
-	description: "Restart Bot",
+	hasPermssion: 0,
+	credits: "AADI SHRIVTASTAV",
+	description: "Restart the Bot",
 	commandCategory: "system",
 	usages: "",
 	cooldowns: 5
@@ -11,6 +11,5 @@ module.exports.config = {
 
 module.exports.run = async ({ api, event, args }) => {
 	const { threadID, messageID } = event;
-	return api.sendMessage(`𝐁𝐎𝐓 𝐈𝐒 𝐑𝐄𝐒𝐓𝐒𝐓  🤖 👈\n\n ● ──────────────────── ●\n\n𝐎𝐰𝐧𝐞𝐫 ➻     𝐀𝐚𝐝𝐢 𝐛𝐚𝐛𝐮\n\n𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐢𝐝 𝐥𝐢𝐧𝐤 𝐀𝐚𝐝𝐢 𝐛𝐚𝐛𝐮 :- ☞\nhttps://www.facebook.com/aadi.babu123
-● ──────────────────── ●\n\n 𝐁𝐎𝐓 𝐑𝐄𝐒𝐓𝐒𝐓 𝐇𝐎 𝐑𝐄𝐇𝐀 𝐇𝐀𝐈 𝐏𝐋𝐙 𝐖𝐀𝐈𝐓`, threadID, () => process.exit(1));
+	return api.sendMessage(`Wait Boss`, threadID, () => process.exit(1));
 }
